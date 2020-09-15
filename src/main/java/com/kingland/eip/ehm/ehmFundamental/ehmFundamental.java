43b3b6874b4e4f2368eb417244af0ccb32a3ae30
Victoria-Meiyu <Victoria.Meiyu.Li@Kingland.com>
2020-09-15 17:37:13 +0800

    Victoria's Ehm practice on 9.14th

--- src/main/java/com/kingland/eip/ehm/ehmFundamental/ehmFundamental.java
+++ src/main/java/com/kingland/eip/ehm/ehmFundamental/ehmFundamental.java
@@ -3,17 +3,17 @@
  * Copyright 2020 Kingland Systems Corporation. All Rights Reserved.
 
  */
-package com.kingland.eip.ehm;
+package com.kingland.eip.ehm.ehmFundamental;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class FundamentalTest {
+public class ehmFundamental {
     /**
      * The number and  abbreviation  of the month
      */
     public static final int Month = 10;
- //1.使用Intellij 创建一个包，包名为ehm,创建一个叫做FundamentalTest的java文件，在其中打印“hello world”。
+ //1.Use Intellij to create a package called EhM, with the Improper printing of "Hello World" from a Java file called Primitive Altest.
     public static void main(String[] args) {
         System.out.println("hello world");
 
@@ -33,8 +33,8 @@ public class FundamentalTest {
             System.out.println("The length of String is Odd number");
         }
     }
-    /*2.使用刚刚创建的java文件写代码，完成以下功能，打印下方的图案，并且写上java doc，
-    在图案的下方打印当前的日期，以及时间（使用LocalDate 和LocalTime）
+    /*2.Write the code using the Java file you just created, do the following, print the pattern below, and say Java Doc,
+        Print the current date and time below the pattern (using LocalDate and LocalTime)
             **********
             *********
             ********
@@ -53,7 +53,8 @@ public class FundamentalTest {
         }
         System.out.println("LocalDate is :" + LocalDate.now() + "LocalTime is :" + LocalTime.now());
     }
-   //3.定义一个不可变int变量代表月份 ， 初始月份为10月，在不同的月份输出对应的月份的英文缩写。
+    //3.An immutable int variable is defined to represent the month,
+    // the initial month is October, in different months output corresponding month abbreviations.
     /**
      * This will prints out the corresponding month abbreviation
      */
@@ -97,13 +98,13 @@ public class FundamentalTest {
                 break;
         }
     }
-  /*
-  4.定义一个方法，这个方法在main函数中调用，在main函数中定义一个字符串“Good Morning”，
-  把字符串作为参数传入定义的方法中，如果字符串含有空格，并且字符串第一个字母为G，
-  则把这个字符串用空格进行分割，将分割后的两个单词打出来,并且一个单词为一行（使用for each的形式），
-  然后将“Good Morning”的长度作为这个方法的返回值，最后在main函数中判断如果长度为偶数，
-  则打印这个字符串的长度，否侧输出“字符串为奇数”
-   */
+
+    /*4.Define a method called in the main function that defines a string "Good Morning,"
+    Pass the string as an argument into the defined method. If the string contains a space and the first letter of the string is G,
+    Divide the string with a space, type out the two separated words, and make one word a line (using the form for each),
+    The length of "Good Morning" will be the return value of this method. Finally, in main function, if the length is even, the length of the string will be printed.
+    If the length is odd, the output will be "odd string".
+     */
     /**
      * If the original String meets the requirements,  every element in it will be printed out and return the he length of it
      */
