edb208d0e7e12ba5ce3db0c28ce96b35bd83eaa8
Victoria-Meiyu <Victoria.Meiyu.Li@Kingland.com>
2020-09-18 17:04:32 +0800

    Test for lambda

--- src/main/java/com/kingland/eip/ehm/Lambda/Tester.java
+++ src/main/java/com/kingland/eip/ehm/Lambda/Tester.java
@@ -1,5 +1,6 @@
 package com.kingland.eip.ehm.Lambda;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,7 +12,7 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Tester {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Artist art = new Artist("Alan", 10, profession.b);
         Artist art2 = new Artist("Alice", 45, profession.a);
         Artist art3 = new Artist("Mike", 63, profession.b);
@@ -23,17 +24,56 @@ public class Tester {
         ArtistList.add(art4);
         Stream<Artist> stream = ArtistList.stream();
 
+        //Find artists whoes name start with A
         System.out.println("1. Find artists whoes name start with A");
         Predicate<Artist> predicate = a -> a.getName().startsWith("A");
         stream.filter(predicate).forEach(a -> System.out.println(a));
 
+        //Get all ages from artists
         System.out.println("2. Get all ages from artists");
-        Stream<Integer> age = ArtistList.stream().map(a -> a.getAge());
-        age.forEach(a -> System.out.println(a));
+        Stream<Integer> allages = ArtistList.stream().map(a -> a.getAge());
+        allages.forEach(a -> System.out.println(a));
 
-        Stream<Integer> getTwoage = ArtistList.stream().map(a -> a.getAge());
+        //Get first two ages from artists
+        System.out.println("3. Get first two ages from artists");
+        Stream<Integer> getTwoages = ArtistList.stream().map(a -> a.getAge());
         AtomicInteger i = new AtomicInteger();
-        getTwoage.filter(a -> i.getAndIncrement() < 2).forEach(a -> System.out.println(a));
+        getTwoages.filter(a -> i.getAndIncrement() < 2).forEach(a -> System.out.println(a));
+
+        //4.Sort the artist list by age in desc.
+
+
+        //5. Print artists' name
+        System.out.println("5. Print artists' name");
+        Stream<String> names = ArtistList.stream().map(a -> a.getName());
+        names.forEach(a -> System.out.println(a));
+        //6. Get all even number ages from artists
+
+        //7. Get max age from artists.
+        Stream<Integer> maxAge = ArtistList.stream().map(a -> a.getAge());
+        System.out.println(maxAge.max(Integer::compareTo).get());
+
+        //8. Get singer whose age is less than 30.
+
+        //9. Get set collection from artists.
+
+        //10. Stream<List<Integer>> inputStream = Stream.of(
+        //Arrays.asList(1),
+        //Arrays.asList(2, 3),
+        // Arrays.asList(4, 5, 6));Return a List which data should be {1,2,3,4,5,6};
+
+
+
+        //2.1. start date 2020/9/13 7:23:23, end date 2020/9/15 13:15:35. Get milliseconds between start date and end date
+        String startDate = "2020/9/13 7:23:23";
+        String  endDate = "2020/9/15 13:15:35";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            System.out.println(sdf.parse(endDate).getTime() -sdf.parse(startDate).getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
     }
