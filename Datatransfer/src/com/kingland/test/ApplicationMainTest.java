0c21de1fe7ca66a48d2c92bef296a7b19bba4e00
Victoria-Meiyu <Victoria.Meiyu.Li@Kingland.com>
2020-10-14 15:30:42 +0800

    data transfer test

---
+++ Datatransfer/src/com/kingland/test/ApplicationMainTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2020 Kingland Systems Corporation. All Rights Reserved.
+ */
+
+package com.kingland.test;
+
+public class ApplicationMainTest {
+
+    //given
+
+    //when
+
+    //then
+
+
+}
